234f4a9d6e15387c77eb19c4bfd55444c33337ba
Derrick Mulinde <djmulinde@gmail.com>
2023-07-16 21:58:05 +0300

    Create application routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  Explore, ForgotPassword, Offers, Profile, SignIn, SignUp, NavBar,
+} from './components/Pages';
+
+const App = () => (
+  <>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </Router>
+  </>
+);
+
+export default App;

d09cdb8f3c575ea478cde8a5ac0186c40d655b99
Derrick Mulinde <djmulinde@gmail.com>
2023-07-16 21:59:11 +0300

    Add custom screen resolutions for better responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '640px',
+      lg: '768px',
+      xl: '1024px',
+      '2xl': '1280px',
+      '3xl': '1440px',
+    },
     extend: {},
   },
   plugins: [],

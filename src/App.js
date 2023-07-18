cb47766147868dee965043bb8be754e346a563e8
Derrick Mulinde <djmulinde@gmail.com>
2023-07-18 21:45:15 +0300

    Add `react-toastify` library
    - This will handle errors and notifications in a more proper way for users
    - Import the ToastContainer into the main component of the application
    - Use the default props for the ToastContainer componet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
   Explore, ForgotPassword, Offers, Profile, SignIn, SignUp, NavBar,
 } from './components/Pages';
@@ -16,6 +18,8 @@ const App = () => (
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </Router>
+
+    <ToastContainer />
   </>
 );
 

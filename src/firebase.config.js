f72c56f0184a66712f329bf45d5b4f6d4b08cee7
Derrick Mulinde <djmulinde@gmail.com>
2023-07-18 21:43:38 +0300

    Update firebase.config.js
    - Import `getFirestore` from `firebase/firestore` to handle database transactions

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
-// import getFirestore from 'firebase/firestore';
-import { getAuth } from '@firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 // export default db;
 
-export default auth;
+export { auth, db };

0b8aafce3865e1ceb9138373e341e8b7dfc4ae32
Derrick Mulinde <djmulinde@gmail.com>
2023-07-20 22:36:24 +0300

    Add more imports

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -5,6 +5,8 @@ import Profile from '../pages/Profile';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import NavBar from './NavBar';
+import PrivateRoutes from './PrivateRoutes';
+import OAuth from './OAuth';
 
 export {
   Explore,
@@ -14,4 +16,6 @@ export {
   SignIn,
   SignUp,
   NavBar,
+  PrivateRoutes,
+  OAuth,
 };

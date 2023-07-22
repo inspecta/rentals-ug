67049ce8bf16d416b1e55a635fa7f2c8395b3abf
Derrick Mulinde <djmulinde@gmail.com>
2023-07-22 17:57:48 +0300

    Import the offers page into the main component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
-  Explore, ForgotPassword, Offers, Profile, SignIn, SignUp, NavBar, PrivateRoutes,
+  Explore, ForgotPassword, Offers, Profile, SignIn, SignUp, NavBar,
+  PrivateRoutes, Category, AddListing,
 } from './components/Pages';
 
 const App = () => (
@@ -11,12 +12,14 @@ const App = () => (
       <NavBar />
       <Routes>
         <Route path="/" element={<Explore />} />
+        <Route path="/add-listing" element={<AddListing />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/category/:categoryName" element={<Category />} />
         <Route path="/profile" element={<PrivateRoutes />}>
           <Route path="/profile" element={<Profile />} />
         </Route>
         <Route path="/profile" element={<Profile />} />
-        <Route path="/offers" element={<Offers />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>

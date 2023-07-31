e0d1cb04788493fc3740fe6ba0835890e9bd2895
Derrick Mulinde <djmulinde@gmail.com>
2023-07-31 22:32:08 +0300

    Import more images

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -2,10 +2,22 @@ import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 import personIcon from '../assets/svg/personIcon.svg';
 import keyboardArrowRightIcon from '../assets/svg/keyboardArrowRightIcon.svg';
 import googleIcon from '../assets/svg/googleIcon.svg';
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import saleCategoryImage from '../assets/jpg/saleCategoryImage.jpg';
+import deleteIcon from '../assets/svg/deleteIcon.svg';
+import bedIcon from '../assets/svg/bedIcon.svg';
+import bathtubIcon from '../assets/svg/bathtubIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 export {
   visibilityIcon,
   personIcon,
   keyboardArrowRightIcon,
   googleIcon,
+  rentCategoryImage,
+  saleCategoryImage,
+  deleteIcon,
+  bedIcon,
+  bathtubIcon,
+  homeIcon,
 };

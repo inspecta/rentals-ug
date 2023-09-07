5c1ef1381db4fd6d1323decccbcb04e1eb06d4c9
Derrick Mulinde <djmulinde@gmail.com>
2023-09-07 22:40:14 +0300

    Update app routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
-  Explore, ForgotPassword, Offers, Profile, SignIn, SignUp, NavBar,
-  PrivateRoutes, Category, AddListing,
+  ForgotPassword, Offers, Profile, SignIn, SignUp, NavBar,
+  PrivateRoutes, Category, AddListing, Listing, Contact, Footer, About,
 } from './components/Pages';
+import Explore from './pages/Explore';
 
 const App = () => (
   <>
@@ -16,16 +17,19 @@ const App = () => (
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/offers" element={<Offers />} />
         <Route path="/category/:categoryName" element={<Category />} />
+        <Route path="/category/:categoryName/:listingId" element={<Listing />} />
         <Route path="/profile" element={<PrivateRoutes />}>
           <Route path="/profile" element={<Profile />} />
         </Route>
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact/:ownerId" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </Router>
 
     <ToastContainer />
+    <Footer />
   </>
 );
 

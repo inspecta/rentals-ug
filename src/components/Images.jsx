695f5e7a7c52f04439fdea4b6ea582b6db918587
Derrick Mulinde <djmulinde@gmail.com>
2023-09-07 22:40:54 +0300

    Export images and icons

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -8,6 +8,12 @@ import deleteIcon from '../assets/svg/deleteIcon.svg';
 import bedIcon from '../assets/svg/bedIcon.svg';
 import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 import homeIcon from '../assets/svg/homeIcon.svg';
+import locationIcon from '../assets/svg/locationIcon.svg';
+import defaultUserIcon from '../assets/png/defaultUser.png';
+import bg1 from '../assets/jpg/bg_1.jpg';
+import bg2 from '../assets/jpg/bg_2.jpg';
+import bg3 from '../assets/jpg/bg_3.jpg';
+import bg4 from '../assets/jpg/bg_4.jpg';
 
 export {
   visibilityIcon,
@@ -20,4 +26,10 @@ export {
   bedIcon,
   bathtubIcon,
   homeIcon,
+  locationIcon,
+  defaultUserIcon,
+  bg1,
+  bg2,
+  bg3,
+  bg4,
 };

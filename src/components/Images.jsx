b0725a7979ec60aee298ddf9b1a99f33a99425c7
Derrick Mulinde <djmulinde@gmail.com>
2023-09-12 13:15:52 +0300

    Update Images.jsx
    - Add more photos and icons for the UI

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -10,10 +10,16 @@ import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 import homeIcon from '../assets/svg/homeIcon.svg';
 import locationIcon from '../assets/svg/locationIcon.svg';
 import defaultUserIcon from '../assets/png/defaultUser.png';
+import profilePhoto from '../assets/png/profilePhoto.png';
 import bg1 from '../assets/jpg/bg_1.jpg';
 import bg2 from '../assets/jpg/bg_2.jpg';
 import bg3 from '../assets/jpg/bg_3.jpg';
 import bg4 from '../assets/jpg/bg_4.jpg';
+import dashboardIcon from '../assets/dashboard-icons/dashboard-1.png';
+import myAccountIcon from '../assets/dashboard-icons/my-account.png';
+import logOutIcon from '../assets/dashboard-icons/logout.png';
+import salesIcon from '../assets/dashboard-icons/house-sales.png';
+import rentalsIcon from '../assets/dashboard-icons/house-rentals.png';
 
 export {
   visibilityIcon,
@@ -28,8 +34,14 @@ export {
   homeIcon,
   locationIcon,
   defaultUserIcon,
+  profilePhoto,
   bg1,
   bg2,
   bg3,
   bg4,
+  dashboardIcon,
+  myAccountIcon,
+  logOutIcon,
+  salesIcon,
+  rentalsIcon,
 };

06c63a2cedd5d3b8bffbd9b1eb17e336e4a9b7e2
Derrick Mulinde <djmulinde@gmail.com>
2023-09-12 13:18:41 +0300

    Update SliderTiles.jsx
    - Update Slider Tiles styles

--- src/components/SliderTiles.jsx
+++ src/components/SliderTiles.jsx
@@ -4,18 +4,23 @@ const SliderTiles = ({ title, description, buttonText }) => (
   <div className="h-screen flex items-center justify-center relative">
     <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" />
     <div className="px-4 bg-white bg-opacity-10 relative text-center h-screen
-      w-screen flex flex-col items-center justify-center"
+      w-screen flex flex-col items-center justify-center lg:px-[200px] xl:px-[300px]"
     >
       <h2 className="
-        text-[40px] mb-8 mt-8 text-white leading-[45px] xl:text-[70px] xl:font-extrabold
+        text-[40px] mb-8 mt-8 text-white leading-[45px] font-bold lg:text-[55px]
+        xl:text-[60px] xl:leading-tight
         "
       >
         {title}
       </h2>
-      <p className="text-gray-200 text-[16px] mb-8 text-center leading-7">
+      <p className="text-gray-200 text-[16px] mb-8 text-center leading-7 xl:text-[20px]">
         {description}
       </p>
-      <p className="uppercase text-[12px] font-semibold bg-[#5ea51e] text-white px-16 py-4 mt-5">
+      <p className="
+        uppercase text-[12px] font-semibold bg-[#5ea51e] text-white px-16 py-4 mt-5
+        xl:text-[15px] cursor-pointer hover:bg-[#36690a]
+        "
+      >
         {buttonText}
       </p>
     </div>
